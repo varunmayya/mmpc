328522c88920b717eebee5365067408aae7066e2
Varun Mayya <varun@RageCore.(none)>
2013-04-11 05:15:17 +0530

    another minor fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ActiveRecord::Base
   resourcify
   default_scope :order => 'name'
-  attr_accessible :available, :img_url, :name, :price, :requires, :type , :company_id
+  attr_accessible :available, :img_url, :name, :price, :requires, :part_type , :company_id
 
   validates :img_url, :format => {:with => %r{\.(gif|jpg|png)$}i, :message => "Please ensure that the image extension is correct."}
-  validates :name, :type , :presence => true, :uniqueness => true
+  validates :name, :presence => true, :uniqueness => true
 
   belongs_to :company
 end
